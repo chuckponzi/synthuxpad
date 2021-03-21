437ba209f41f03b6781170d72587bb20988b37c8
chuckponzi <70590675+chuckponzi@users.noreply.github.com>
2021-03-21 14:28:25 +0700

    Debugged Part Editor

--- src/components/InputPanel/EditPart/index.js
+++ src/components/InputPanel/EditPart/index.js
@@ -1,7 +1,7 @@
 // index.js (src/components/InputPanel/EditPart)
 
 //>>>>> PACKAGES <<<<<//
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //>>>>> SCSS STYLES <<<<<//
 import "./styles.scss";
@@ -9,36 +9,51 @@ import "./styles.scss";
 //>>>>> COMPONENT FUNTCION <<<<<//  
 const EditPart = ({ onSceneDisp, sceneState }) => { 
 
-    const [livepart, setLivePart] = useState(false) 
+    const [livepart, setLivePart] = useState(false); 
+    useEffect(() => {
+        const resetLive = () => {
+            if (!sceneState.parts) {
+                setLivePart(false);
+            };
+        };
+        resetLive();
+    }, [sceneState]);
 
-    //>>>>> Return <<<<<//
+    //>>>>> JSX Return <<<<<//
     return (
         <div
             id="EditPart-container"
-            className="Base-block-flexCol"
+            className="Base-block"
         >
-            <div className="Base-flexedCol-block">
-                <h3>CLICK TO EDIT</h3>
-                {sceneState.parts.map(item => {
-                    return (
-                        <p
-                            onClick={() => setLivePart(item.id)}
-                        >{item.id + " " + item.partName}</p>
-                        )
-                })}
-            </div>
-            {livepart || livepart === 0 ? (
-                <div className="Base-flexedCol-block">
-                    <h3>CURRENT</h3>
-                    <p>{"Editing Part " + livepart}</p>
-                    <p>{"Group: " + sceneState.parts[livepart]["type"]}</p>
-                    <p>{"Part Name: " + sceneState.parts[livepart]["partName"]}</p>
-                    <p>
-                        {"Position: (" +
-                        sceneState.parts[livepart]["position"]["x"] + "," +
-                        sceneState.parts[livepart]["position"]["y"] + "," +
-                        sceneState.parts[livepart]["position"]["z"] + ")"}
-                    </p>
+            {sceneState.parts ? (
+                <div className="Base-block-flexCol" >
+                    <div className="Base-flexedCol-block" >
+                        <h3>PART LIST - CLICK TO EDIT</h3>
+                        {sceneState.parts.map(item => {
+                            return (
+                                <p
+                                    className="EditPart-link"
+                                    onClick={() => setLivePart(item.id - 1)}
+                                >{item.id + " " + item.partName}</p>
+                            )
+                        })}
+                    </div>
+                    <div className="Base-flexedCol-block" >
+                        {livepart || livepart === 0 ? (
+                            <div className="Base-block-flexCol" >
+                                <h3>SELECTED PART</h3>
+                                <p>{"Editing Part " + (livepart+1)}</p>
+                                <p>{"Group: " + sceneState.parts[livepart]["type"]}</p>
+                                <p>{"Part Name: " + sceneState.parts[livepart]["partName"]}</p>
+                                <p>
+                                    {"Position: (" +
+                                        sceneState.parts[livepart]["position"]["x"] + "," +
+                                        sceneState.parts[livepart]["position"]["y"] + "," +
+                                        sceneState.parts[livepart]["position"]["z"] + ")"}
+                                </p>
+                            </div>
+                        ) : (null)}
+                    </div>
                 </div>
             ) : (null)}            
         </div>
